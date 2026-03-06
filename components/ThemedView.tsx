33f26785438edb430f8b30221d99dc5cd8519a56
Andrew Sutiono <sutionoandrew@gmail.com>
2026-03-06 15:12:28 +0700

    added safe area view into the ThemedView

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,22 +1,43 @@
 import { View, useColorScheme, ViewProps, StyleProp, ViewStyle } from 'react-native'
 import { Colors } from '../constants/Colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = ViewProps & {
     style?: StyleProp<ViewStyle>
+    safe?: boolean
 }
 
-export default function ThemedView({ style, ...props }: Props) {
+export default function ThemedView({ style, safe = false, ...props }: Props) {
     const colorScheme = useColorScheme()
     const themeMode = colorScheme === "dark" ? "dark" : "light"
     const theme = Colors[themeMode]
 
+    if (!safe) {
+        return (
+            <View
+                style={[
+                    { backgroundColor: theme.background },
+                    style
+                ]}
+                {...props}
+            />
+        )
+    }
+
+    const insets = useSafeAreaInsets();
+
     return (
         <View
             style={[
-                { backgroundColor: theme.background },
+                {
+                    backgroundColor: theme.background,
+                    paddingTop: insets.top,
+                    paddingBottom: insets.bottom
+                },
                 style
             ]}
             {...props}
         />
     )
+
 }
